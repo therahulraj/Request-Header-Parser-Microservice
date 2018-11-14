a3abfdb55ac3940012c3f281f84f48869e5bf75c
rahulraj16mar <31818361+rahulraj16mar@users.noreply.github.com>
2018-11-15 02:37:15 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const useragent = require('express-useragent');
 
 var port = process.env.PORT || 3000;
@@ -8,6 +9,11 @@ var app = express();
 app.use(useragent.express());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 app.get('/api/whoami', (req, res) => {
   res.send({
     ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress,
